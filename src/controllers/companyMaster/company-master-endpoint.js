863d788cf04e891993aa8bf41ffe28ff65b8ab8c
Dmitri Lipski <dlipski@klika-tech.com>
2020-01-30 10:05:29 +0300

    Update deleteMasterFromCompany

--- src/controllers/companyMaster/company-master-endpoint.js
+++ src/controllers/companyMaster/company-master-endpoint.js
@@ -59,8 +59,28 @@ export const makeCompanyMasterEndpointHandler = ({ companies }) => {
 	}
 
 	async function deleteMasterFromCompany (httpRequest) {
-		const { id: companyId, mid: masterId } = httpRequest.pathParams || {};
+		const { id: companyId } = httpRequest.pathParams || {};
+		let { body } = httpRequest;
+		if (!body) {
+			return makeHttpError({
+				statusCode: 400, // TODO: Replace with custom error
+				errorMessage: 'Bad request. No POST body.'
+			});
+		}
+
+		if (typeof httpRequest.body === 'string') {
+			try {
+				body = JSON.parse(body);
+			} catch(e) {
+				return makeHttpError({
+					statusCode: 400,  // TODO: Replace with custom error
+					errorMessage: 'Bad request. POST body must be valid JSON.'
+				});
+			}
+		}
+
 		try {
+			const { id: masterId } = body;
 			const result = await companies.deleteMaster(companyId, masterId);
 			return {
 				headers: {
